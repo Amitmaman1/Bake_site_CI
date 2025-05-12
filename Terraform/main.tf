5b8ee9b31ec2de728b893813da39469d5b72a586
Amitmaman1 <amitmaman1235@gmail.com>
2025-05-12 19:45:29 +0300

    test

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,4 +1,10 @@
 terraform {
+  backend "s3" {
+    bucket = "myterramamanbucket"  
+    key    = "ec2/terraform.tfstate"
+    region = "us-east-1"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -13,18 +19,12 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-resource "random_pet" "name" {
-  length    = 2
-  separator = "-"
-}
-
 resource "aws_instance" "app_server" {
   ami           = "ami-0f88e80871fd81e91"
   instance_type = "t2.micro"
-  user_data = file("${path.module}/userdata.sh")
+  user_data     = file("${path.module}/userdata.sh")
 
   tags = {
-    Name = "my_web-${random_pet.name.id}"
+    Name = "my_web"
   }
-  
 }
